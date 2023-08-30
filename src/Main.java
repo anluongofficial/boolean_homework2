602d53875f0c79a30f56c2387b732bfff5dfc77b
luong.an <luong.an@outlook.com>
2023-08-30 07:40:46 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean shining = true;
+
+        int time = 20;
+        if (shining == true && time >=10 && time <= 16)
+
+
+            System.out.println("Please use sunscreen" +
+                    "if necessary");
+    }
+}
